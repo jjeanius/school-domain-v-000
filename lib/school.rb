14db23c37e7a496bf14701079cf200419569de25
Judy McGoldrick <jjeanius@verizon.net>
2018-01-03 02:32:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,11 +3,30 @@ class School
   attr_accessor :add_student, :grade, :sort
   attr_reader
 
-  def initialize (name)
+
+
+  def initialize (name, roster)
     @name = name
+    @roster = {}
   end
 
-  rost = { }
+
+  def add_student=(name, grade)
+    @name=name
+    @grade=grade
+    roster [grade] << grade
+    roster [student] << name
+
+  end
+
+  def grade=(grade)
+    @grade=grade
+
+  end
+
+  def sort=(sort)
+    @sort=sort
+  end
 
 
 end
